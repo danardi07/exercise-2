b8f1f2d53f512dc4d187d33b7724e85c4eb5c777
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2024-05-31 14:36:22 +0700

    membuat title page 1

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('audio').addEventListener('play', function() {
+  document.getElementById('video').pause();
+});
+
+document.getElementById('video').addEventListener('play', function() {
+  document.getElementById('audio').pause();
+});
+
+function kirimSurvei() {
+  const nama = document.getElementById('nama').value;
+  const nim = document.getElementById('nim').value;
+  const fasilitas = document.querySelector('input[name="fasilitas"]:checked').value;
+  const saran = document.getElementById('saran').value;
+
+  const formData = {
+    nama: nama,
+    nim: nim,
+    fasilitas: fasilitas,
+    saran: saran
+  };
+
+  console.log(formData);
+
+  alert(`Nama: ${nama}\nNIM: ${nim}\nPenilaian Fasilitas: ${fasilitas}\nSaran: ${saran}`);
+}
